4fb2cdef66dbff0cf9eff22faf6775c51f8cd236
AdderStrike <chemeng133@gmail.com>
2025-06-10 14:22:18 -0500

    Finished BlueShift

--- assist.js
+++ assist.js
@@ -1,1 +1,61 @@
+function blueShift(color) {
+    //console.log("Starting Color for Blue Shift: "+JSON.stringify(color)[1]);
+    if (JSON.stringify(color)[1]=="#"){
+        //console.log("Choosing HEXER");
+        color = hexToRGB(color);
+    } else if (JSON.stringify(color)[1]=="r") {
+        //console.log("Choosing WARPER");
+        color = rgbCSSWarp(color);
+    } else {
+        throw("BlueShift Error:BlueShift has encountered an uncomputed color error and is not prepared.")
+    }
 
+    color[0]*=3/4;
+
+    if (color[1]<113){
+        color[1] +=15;
+    } else if (color[1]>143){
+        color[1]-=15;
+    }
+
+    if (color[2]+40>=255){
+        color[2]=255;
+    } else {
+        color[2]+=40;
+    }
+
+    for (var i=0;i<color.length;i++){
+        color[i]=Math.round(color[i]);
+    }
+  //console.log(color);
+
+  return rgbToHex(color);
+
+}
+
+function rgbToHex(color){
+const rgb = (color[0] << 16) | (color[1] << 8) | (color[2] << 0);
+  return '#' + (0x1000000 + rgb).toString(16).slice(1);
+}
+
+function hexToRGB(color){
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
+  
+  result = result.slice(1,4);
+  for (var i=0;i<result.length;i++) {
+    result[i] = parseInt(result[i],16);
+  }
+  return result;
+}
+
+function rgbCSSWarp(color){
+    let fracArray = JSON.stringify(color).split(",");
+
+    for (var i =0;i<fracArray.length;i++){
+        fracArray[i] = fracArray[i].replace(/\D/g,"");
+        fracArray[i] = Number(fracArray[i]);
+    }
+    
+    //console.log(fracArray);
+    return fracArray;
+}
